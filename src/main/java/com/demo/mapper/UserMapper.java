3011150093cc831ebebd2260877f68b1d5cde60f
biubiu <wbbaijq@centaline.com.cn>
2020-05-20 15:31:41 +0800

    first commit

---
+++ src/main/java/com/demo/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.demo.mapper;
+
+import com.demo.entity.User;
+
+import java.util.List;
+
+/**
+ * UserMapper
+ *
+ * @author biubiu
+ */
+public interface UserMapper {
+
+    List<User> queryAll();
+}
---
+++ src/main/java/com/demo/service/UserService.java
@@ -0,0 +1,14 @@
+package com.demo.service;
+
+import com.demo.entity.User;
+
+import java.util.List;
+
+/**
+ * UserService
+ *
+ * @author biubiu
+ */
+public interface UserService {
+    public List<User> findAll();
+}
